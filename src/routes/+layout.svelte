f65248a66b333b4b43964f4365d259abf27d9213
Charif Mews <charif@westaysafe.com>
2023-09-30 13:24:54 +0200

    Add scaffolded client and API routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,4 @@
+<header>
+    Some header
+</header>
+<slot></slot>
---
+++ src/routes/api/qlog/export/+server.ts
@@ -0,0 +1,7 @@
+import { error } from '@sveltejs/kit';
+
+
+/** @type {import('./$types').RequestHandler} */
+export function GET({ url }) {
+	return new Response(String("Some data"));
+}
