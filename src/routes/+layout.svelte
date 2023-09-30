fe911824ebaaecb38a564bff764d9c02fea2e169
Charif Mews <charif@westaysafe.com>
2023-09-30 14:18:51 +0200

    Working header & new data market page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,18 @@
     import { defaultEvmStores, signerAddress, signer } from 'ethers-svelte';
 </script>
 
-<header>
-    Some header
+<header class="bg-red-500 w-full flex items-center justify-between">
+    <div class="flex items-center">
+        <a href="/">
+            <img class="h-[20px]" src="/qlog.svg" alt="qlog.ai">
+        </a>
+        <a href="/questions">
+            Your Questions
+        </a>
+        <a href="/data-market">
+            Data market
+        </a>
+    </div>
+    <button>Wallet Sign In</button>
 </header>
 <slot></slot>
---
+++ src/routes/data-market/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Data Market</h1>
