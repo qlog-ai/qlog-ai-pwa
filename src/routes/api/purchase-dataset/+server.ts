8eb53e368474921700fb181a83cee3b891c8c742
Charif Mews <charif@westaysafe.com>
2023-09-30 23:53:11 +0200

    Fix backend endpoint

--- src/routes/api/purchase-dataset/+server.ts
+++ src/routes/api/purchase-dataset/+server.ts
@@ -1,4 +1,4 @@
-import { error } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 import mysql from '$lib/server/db/mysql';
 import { questions } from '$lib/server/schema/questions';
 import { eq, desc } from 'drizzle-orm';
@@ -6,14 +6,16 @@ import { getVerifiedUserAddress } from '$lib/route_utils';
 import { BEARER_TOKEN } from '$env/static/private';
 
 export async function POST({ request, cookies }) {
-    // Bearer token check
+	// Bearer token check
 	const bearer_token = request.headers.get('Authorization')?.split(' ')[1];
 
 	if (bearer_token === BEARER_TOKEN) {
 		const { purchaseWalletAddress, datasetAddress } = await request.json();
-        // some magic
-        
-        return new Response(String('22'));
-    }
+		// some magic
+
+		return json({
+			questions: 22
+		});
+	}
 	return new Response(String('purchased'));
 }
