23533ecdcecb58f995552802025cd5719cf835c7
Charif Mews <charif@westaysafe.com>
2023-10-01 08:07:34 +0200

    Working decentralised storage endpoint for Chainlink functions :D

--- src/routes/api/purchase-dataset/[purchaser_address]/[dataset_address]/+server.ts
+++ src/routes/api/purchase-dataset/[purchaser_address]/[dataset_address]/+server.ts
@@ -2,17 +2,28 @@ import { error, json } from '@sveltejs/kit';
 import mysql from '$lib/server/db/mysql';
 import { questions } from '$lib/server/schema/questions';
 import { eq, desc } from 'drizzle-orm';
-import { getVerifiedUserAddress } from '$lib/route_utils';
-import { BEARER_TOKEN } from '$env/static/private';
+import { BEARER_TOKEN, WEB3_STORAGE_API_TOKEN } from '$env/static/private';
+import { Web3Storage } from 'web3.storage';
+
+const web3storage = new Web3Storage({ token: WEB3_STORAGE_API_TOKEN })
 
 export async function GET({ request, params }) {
 	// Bearer token check
 	const bearer_token = request.headers.get('authorization')?.split(' ')[1];
 	if (bearer_token === BEARER_TOKEN) {
-		// Encrypt dataset with public key and push to IPFS?
+		const { purchaser_address, dataset_address } = params;
+		const dataset = JSON.stringify(
+			await mysql
+				.select()
+				.from(questions)
+				.where(eq(questions.wallet_address, dataset_address))
+				.orderBy(desc(questions.updated_at))
+		)
+
+        const cid = await web3storage.put([new File([dataset], `${purchaser_address}-${dataset_address}.json`, { type: 'application/JSON' })])        
 
 		return json({
-			questions: 22
+			cid: cid
 		});
 	} else {
 		throw error(401, { message: 'Unauthorized'})
