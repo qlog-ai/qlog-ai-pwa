d8d88fca03ceb055e60abd76c2a1c26d15006152
Charif Mews <charif@westaysafe.com>
2023-10-01 03:34:48 +0200

    Try again with web3 storage

--- src/routes/api/test/+server.ts
+++ src/routes/api/test/+server.ts
@@ -3,18 +3,18 @@ import mysql from '$lib/server/db/mysql';
 import { questions } from '$lib/server/schema/questions';
 import { eq, desc } from 'drizzle-orm';
 import { getVerifiedUserAddress } from '$lib/route_utils';
-import { BEARER_TOKEN } from '$env/static/private';
-import { createHelia } from 'helia';
-import { json } from '@helia/json';
+import { Web3Storage } from 'web3.storage';
+import { WEB3_STORAGE_API_TOKEN } from '$env/static/private';
+
+const web3storage = new Web3Storage({ token: WEB3_STORAGE_API_TOKEN })
 
 export async function GET({ request, params }) {		
         
-        const helia = await createHelia()
-        const j = json(helia)
+        const cid = await web3storage.put([new File(['hello world of smart con, will this work hopefully?'], 'someawesomedataset.txt', { type: 'text/plain' })])        
+        // const myImmutableAddress = await j.add({ hello: 'smart con world' })
         
-        const myImmutableAddress = await j.add({ hello: 'smart con world' })
-        
-        return new Response(String(myImmutableAddress));
+        // return new Response(String(myImmutableAddress));
+        return new Response(String(cid));
 	
 }
 
